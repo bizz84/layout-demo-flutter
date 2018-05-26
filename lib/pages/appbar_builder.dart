1a5ef7755542db77156d5dbfa16f1eabdd72e21b
Andrea Bizzotto <bizz84@gmail.com>
2018-05-26 20:07:22 +0100

    Remove link to website and url_launcher as some users reported problems using it

--- lib/pages/appbar_builder.dart
+++ lib/pages/appbar_builder.dart
@@ -1,27 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:layout_demo_flutter/layout_type.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class AppBarBuilder {
-  static void _launchURL() async {
-    const url = 'https://codingwithflutter.com';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
 
   static AppBar build({LayoutType layoutType, PreferredSize bottom}) {
     return AppBar(
       title: Text(layoutName(layoutType)),
       elevation: 1.0,
-      actions: [
-        IconButton(
-          icon: Icon(Icons.info_outline),
-          onPressed: _launchURL,
-        )
-      ],
       bottom: bottom,
     );
   }
